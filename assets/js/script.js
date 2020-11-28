2ea9388c7194af05108543ddc7285c2111b4f06b
BigSky <skylarromero4@gmail.com>
2020-11-27 20:48:17 -0700

    added buttons to tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
+var taskIdCounter = 0;
 
 
 var taskFormHandler = function(event) {
@@ -22,11 +23,61 @@ var taskFormHandler = function(event) {
 var createTaskE1 = function(taskDataObj) {
     var listItemE1 = document.createElement("li");
     listItemE1.className = "task-item";
+
+    listItemE1.setAttribute("data-task-id", taskIdCounter);
+
     var taskInfoE1 = document.createElement("div");
     taskInfoE1.className = "task-info";
     taskInfoE1.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type +"</span>";
     listItemE1.appendChild(taskInfoE1);
+    var taskActionE1 = createTaskActions(taskIdCounter);
+    listItemE1.appendChild(taskActionE1);
+    
+
     tasksToDoE1.appendChild(listItemE1);
 
+    taskIdCounter++;
+
 }
+
+var createTaskActions = function(taskId) {
+    var actionContainerE1 = document.createElement("div");
+    actionContainerE1.className = "task-actions";
+    // edit button
+    var editButtonE1 = document.createElement("button");
+    editButtonE1.textContent = "Edit";
+    editButtonE1.className = "btn edit-btn";
+    editButtonE1.setAttribute("data-task-id", taskId);
+
+    actionContainerE1.appendChild(editButtonE1);
+
+    // delete button
+    var deleteButtonE1 = document.createElement("button");
+    deleteButtonE1.textContent = "Delete";
+    deleteButtonE1.className = "btn delete-btn";
+    deleteButtonE1.setAttribute("data-task-id", taskId);
+
+    actionContainerE1.appendChild(deleteButtonE1);
+
+    var statusSelectE1 = document.createElement("select");
+    statusSelectE1.className = "select-status";
+    statusSelectE1.setAttribute("name", "status-change");
+    statusSelectE1.setAttribute("data-task-id", taskId);
+
+    actionContainerE1.appendChild(statusSelectE1);
+
+    var statusChoices = ["To Do", "In Progress", "Completed"];
+
+    for (var i=0; i <statusChoices.length; i++) {
+        // option element
+        var statusOptionE1 = document.createElement("option");
+        statusOptionE1.textContent = statusChoices[i];
+        statusOptionE1.setAttribute("value", statusChoices[i]);
+
+        statusSelectE1.appendChild(statusOptionE1);
+    }
+
+    return actionContainerE1;
+
+};
 formE1.addEventListener("submit", taskFormHandler);
