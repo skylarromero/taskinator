b8ea14d12a1dec1836d12b6f1a0acd1df30553ff
BigSky <skylarromero4@gmail.com>
2020-11-29 19:02:57 -0700

    delete works!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 var formE1 = document.querySelector("#task-form");
 var tasksToDoE1 = document.querySelector("#tasks-to-do");
 var taskIdCounter = 0;
-
+var pageContentE1 = document.querySelector("#page-content");
 
 var taskFormHandler = function(event) {
 
@@ -80,4 +80,20 @@ var createTaskActions = function(taskId) {
     return actionContainerE1;
 
 };
+
+var taskButtonHandler = function(event) {
+    console.log(event.target);
+
+    if ( event.target.matches(".delete-btn")) {
+        var taskId = event.target.getAttribute("data-task-id");
+        deleteTask(taskId);
+    }
+};
+
+var deleteTask = function(taskId) {
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    taskSelected.remove();
+    
+};
 formE1.addEventListener("submit", taskFormHandler);
+pageContentE1.addEventListener("click", taskButtonHandler);
